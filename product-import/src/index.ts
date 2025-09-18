669b291214e865605acf8cfce1c9cf466bc0409a
Bjorna Kalaja <bjk@foryouandyourcustomers.com>
2025-09-18 17:27:24 +0200

    expose productImport endpoint

--- product-import/src/index.ts
+++ product-import/src/index.ts
@@ -1,12 +1,25 @@
 import {importProducts} from "./ProductImport";
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/productImport', async (req: Request, res: Response) => {
+    const action = req.body;
+    console.info('New action received', action);
+
+
+    // build additional resource update action
+    importProducts().then(result => console.log(result));
+    res.status(200).json({ actions: [] });
+});
 
 function main(): void {
     console.log("Witt POC Import Adapter");
-
-    // Retrieve Project information and output the result to the log
-
-    importProducts().then(result => console.log(result));
-
 }
 
 main();
